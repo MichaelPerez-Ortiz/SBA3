8f986474670e9a48f43052aa21f37b2f13f916b0
Michael Pérez-Ortiz <perezortizm7@gmail.com>
2025-05-21 15:37:48 -0400

    Added core functionality and fixed min name length error

--- script.js
+++ script.js
@@ -20,9 +20,103 @@ const game = {
         { text: "No", goto: "examine_start" },
     ]
 
-}
+},
+
+yes: {
+    text:"You are now responsible for searching for new advances in technology to help keep our planet alive. You Have seven days to scour the galaxy for a way to save Jupiter.( Hands you the full report) When you are ready go get your ship.",
+    choices: [
+        { text: "Go to your ship", goto: "ship" },
+        { text: "Read the full report", goto: "report" },
+    ]
+},
+
+
+
+
+
+
 
 
 };
 
 
+function resetGame() {
+    inventory = [];
+    inventoryList.innerHTML = '';
+    updateStart('start');
+}
+
+
+playerForm.addEventListener('submit', startGame);
+
+         window.addEventListener('load', () => {
+    if (localStorage.getItem('playerName')) {
+        playerNameInput.value = localStorage.getItem('playerName');
+    }
+});
+
+
+playerNameInput.addEventListener('input', validateName);
+
+function validateName(event) {
+        const name = event.target.value;
+        const errorDiv = document.querySelector('.error') || document.createElement('div');
+        errorDiv.className = 'error';
+
+    if (name.length < 2) {
+        errorDiv.textContent = 'Name must be at least 2 characters long';
+    if (!document.querySelector('.error')) {
+         playerForm.appendChild(errorDiv);
+    }
+     } else {
+         errorDiv.remove();
+    }
+}
+
+function startGame(event) {
+    event.preventDefault();
+    playerName = playerNameInput.value;
+    localStorage.setItem('playerName', playerName);
+
+        startScreen.classList.add('hidden');
+        gameScreen.classList.remove('hidden');
+
+    updateStart('start');
+}
+
+function updateStart(startId) {
+    const start = game[startId];
+    currentPosition = startId;
+
+        storyText.textContent = start.text;
+        choicesDiv.innerHTML = '';
+
+        
+const fragment = document.createDocumentFragment();
+
+start.choices.forEach(choice => {
+    if (!choice.requires || inventory.includes(choice.requires)) {
+        const choiceBtn = choiceTemplate.content.cloneNode(true).querySelector('button');
+        choiceBtn.textContent = choice.text;
+        choiceBtn.addEventListener('click', () => {
+
+    if (choice.action) choice.action();
+         updateStart(choice.goto);
+    });
+        fragment.appendChild(choiceBtn);
+    }
+    });
+
+         choicesDiv.appendChild(fragment);
+}
+
+function addToInventory(item) {
+    if (!inventory.includes(item)) {
+            inventory.push(item);
+
+        const li = document.createElement('li');
+        li.textContent = item;
+
+        inventoryList.appendChild(li);
+    }
+}
